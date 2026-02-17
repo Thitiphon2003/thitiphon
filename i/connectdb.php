6184022605e81e25ff868fbdad9361cccf8b2f89
Thitiphon2003 <66010914140@msu.ac.th>
2026-02-17 19:44:43 +0700

    a

--- i/connectdb.php
+++ i/connectdb.php
@@ -1,7 +1,7 @@
 <?php
 		$host = "localhost";
 		$user = "root";
-		$pwd = "";
+		$pwd = "r660109";
 		$db = "4140db";
 		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 		mysqli_query($conn, "SET NAMES utf8");
