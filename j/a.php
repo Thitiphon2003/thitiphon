0a183ae242ff34a04b9e44a9931ac5ddd4914461
Thitiphon2003 <66010914140@msu.ac.th>
2026-02-18 13:29:38 +0700

    a

---
+++ j/a.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ฐิติพล มหานาม (บอส) </title>
+</head> 
+
+<body>
+<h1> งาน j ฐิติพล มหานาม (บอส) <br>66010914140 </h1>
+<img src = "1.jpg"class="profile-img" height="500px"><img src = "2.jpg"class="profile-img" height="500px">
+
+</body>
+</html>
